76957df7848714a5b3290d80ba80f8cbfe11bf1c
cynthwu <83854994+cynthwu@users.noreply.github.com>
2021-05-16 21:00:39 -0700

    Update pluralize_gift.R

--- R/pluralize_gift.R
+++ R/pluralize_gift.R
@@ -11,13 +11,3 @@
 #' @import english
 #'
 #' @export
-pluralize_gift <- function(gift) {
-  
-    if (str_detect(gift, "oo") == TRUE) {
-      return(str_replace(gift, "oo", "ee"))
-  } else if (str_detect(gift, "$y") == TRUE) {
-      return(str_replace("$y", "ies"))
-  } else {
-      return(paste0(gift, "s"))
-  }
-}
