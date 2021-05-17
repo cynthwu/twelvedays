116e6cb36bf36e6b98d4578fe4e80dfa712198c8
cynthwu <83854994+cynthwu@users.noreply.github.com>
2021-05-16 20:04:25 -0700

    Update pluralize_gift.R

--- R/pluralize_gift.R
+++ R/pluralize_gift.R
@@ -8,13 +8,16 @@
 #' @import dplyr
 #' @import glue
 #' @import purrr
+#' @import english
 #'
 #' @export
-pluralize_gift <- function(gift){
-
-gift <- gift %>%
-  str_replace()
-
-return(gift)
-
+pluralize_gift <- function(gift) {
+  
+    if (str_detect(gift, "oo") == TRUE) {
+      return(str_replace(gift, "oo", "ee"))
+  } else if (str_detect(gift, "$y") == TRUE) {
+      return(str_replace("$y", "ies"))
+  } else {
+      return(paste0(gift, "s"))
+  }
 }
