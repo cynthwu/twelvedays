3235d6fc1900906460a1a7aa3fcd0fc0e8fe564a
cynthwu <83854994+cynthwu@users.noreply.github.com>
2021-05-22 03:50:19 -0700

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,22 @@ library(testthat)
 library(twelvedays)
 
 test_check("twelvedays")
+
+test_that("the pluralize_gift function should only have one argument", {
+  expect_error(pluralize_gift("bunny", "carrot"))
+})
+
+test_that("the num arguement in the make_phrase function should be numeric", {
+  expect_error(make_phrase(num = "hello", 
+                           num_word = "six", 
+                           item = "ballon", 
+                           verb = "flying", 
+                           adjective = "green", 
+                           location = "up in the sky"))
+})
+
+test_that("all of the arguments in the sing_line function are required, not optional", {
+  expect_error(sing_day(line = 5, phrase_col = Full.Phrase))
+})
+
+# Note: Sorry I didn't know we were supposed to put the challenge in this file. I inputted the unit tests in each function's R document instead. 
